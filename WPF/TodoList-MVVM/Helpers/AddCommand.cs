7607f21d3c6af094993c8d7e672bb617406ae5ba
Ramesh Verma <rsverma333@gmail.com>
2023-12-28 12:47:52 +0530

    WPF MVVM TodoList

---
+++ WPF/TodoList-MVVM/Helpers/AddCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TodoList.ViewModels;
+
+namespace TodoList.Helpers
+{
+    internal class GenericCommand : ICommand
+    {
+        private readonly Action<object?> executeAction;
+
+        public event EventHandler? CanExecuteChanged;
+        public GenericCommand(Action<object?> executeAction)
+        {
+            this.executeAction = executeAction;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            executeAction?.Invoke(parameter);
+        }
+    }
+}
---
+++ WPF/TodoList-MVVM/ViewModels/TodoViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TodoList.Helpers;
+using TodoList.Models;
+
+namespace TodoList.ViewModels
+{
+    public class TodoViewModel : ChangeNotifier
+    {
+        public string Title { get { return $"Todo({todoModels.Count(x=>!x.IsDone)}) Completed({todoModels.Count(x => x.IsDone)})"; }  }
+        private ObservableCollection<TodoModel> todoModels = new ObservableCollection<TodoModel>();
+
+		public ObservableCollection<TodoModel> TodoModels
+		{
+			get { return todoModels; }
+			set { todoModels = value; }
+		}
+		private string _newTodo;
+
+        public string NewTodo
+		{
+			get { return _newTodo; }
+            set { _newTodo = value; OnPropertyChanged(nameof(NewTodo)); }
+		}
+
+        public ICommand AddCommand { get; set; }
+        internal void AddTodo(object? param)
+        {
+           if(string.IsNullOrEmpty(_newTodo)) return;
+           TodoModel todo = new TodoModel { Title = _newTodo };
+            todo.PropertyChanged += Todo_PropertyChanged;
+            TodoModels.Add(todo);
+            NewTodo = string.Empty;
+            OnPropertyChanged(nameof(Title));
+        }
+
+        private void Todo_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(Title));
+        }
+
+        public TodoViewModel()
+        {
+            AddCommand = new GenericCommand(AddTodo);
+        }
+    }
+}
