6f339652d10b368427e6a5eab6da231aaf0a2522
Ramesh Verma <rsverma333@gmail.com>
2024-02-11 22:29:36 +0530

    Add Entity Framework to project

--- API/WEB API/SuperHeroAPI/Program.cs
+++ API/WEB API/SuperHeroAPI/Program.cs
@@ -1,5 +1,8 @@
 global using SuperHeroAPI.Models;
+global using Microsoft.EntityFrameworkCore;
 using SuperHeroAPI.Services;
+using SuperHeroAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
