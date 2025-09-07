ecdc15b601a7460fe80772e6abeb0400535760ca
CodeWithRSV <rsverma3333@gmail.com>
2025-09-07 22:25:15 +0530

    Added state pattern

---
+++ design-patterns/DemoApp/State/Document.cs
@@ -0,0 +1,26 @@
+﻿public class DocumentContext
+{
+    private IState _state;
+
+    public DocumentContext(IState state)
+    {
+        this.TransitionTo(state);
+    }
+
+    // The Context allows changing the State of object at runtime.
+    public void TransitionTo(IState state)
+    {
+        Console.WriteLine($"Transition to {state.GetType().Name}.");
+        this._state = state;
+    }
+
+    public void Publish(string currentUser)
+    {
+        this._state.HandlePublish(currentUser);
+    }
+
+    public void Edit(string currentUser)
+    {
+        this._state.HandleEdit(currentUser);
+    }
+}
---
+++ design-patterns/DemoApp/State/State.cs
@@ -0,0 +1,78 @@
+﻿public interface IState
+{
+    void HandlePublish(string currentUser);
+
+    void HandleEdit(string currentUser);
+}
+
+public class DraftState : IState
+{
+    protected DocumentContext _context;
+    public DraftState(DocumentContext context)
+    {
+        _context = context;
+    }
+    public virtual void HandlePublish(string currentUser)
+    {
+        if (currentUser != "Author")
+        {
+            Console.WriteLine($"{currentUser} cannot publish in Draft state. Only Author can publish.");
+            return;
+        }
+        _context.TransitionTo(new ModerationState(_context));
+        Console.WriteLine("Document sent to admin for moderation");
+    }
+    public virtual void HandleEdit(string currentUser)
+    {
+        if (currentUser != "Author")
+        {
+            Console.WriteLine($"{currentUser} cannot edit in Draft state. Only Author can edit.");
+            return;
+        }
+        Console.WriteLine($"Document edited by author");
+    }
+}
+
+public class ModerationState : IState
+{
+    protected DocumentContext _context;
+    public ModerationState(DocumentContext context)
+    {
+        _context = context;
+    }
+    public virtual void HandlePublish(string currentUser)
+    {
+        if (currentUser != "Admin")
+        {
+            Console.WriteLine($"{currentUser} cannot publish in Moderation state. Only admin can publish.");
+            return;
+        }
+        _context.TransitionTo(new PublishedState(_context));
+        Console.WriteLine("Document published by admin");
+    }
+    public virtual void HandleEdit(string currentUser)
+    {
+        if (currentUser != "Admin")
+        {
+            Console.WriteLine($"{currentUser} cannot edit in moderation state. Only admin can edit.");
+            return;
+        }
+        Console.WriteLine($"Document edited by admin");
+    }
+}
+public class PublishedState : IState
+{
+    protected DocumentContext _context;
+    public PublishedState(DocumentContext context)
+    {
+        _context = context;
+    }
+    public virtual void HandlePublish(string currentUser)
+    {
+        Console.WriteLine("Invalid operation. Document is already published.");
+    }
+    public virtual void HandleEdit(string currentUser)
+    {
+        Console.WriteLine("Invalid operation. Published document cannot be edited.");
+    }
+}
