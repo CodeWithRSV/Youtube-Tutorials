d65bd4c2ef30e517526fcb76d5a930002e347a2d
CodeWithRSV <rsverma3333@gmail.com>
2025-09-09 14:47:19 +0530

    Added Strategy Pattern

---
+++ design-patterns/DemoApp/Strategy/Context.cs
@@ -0,0 +1,12 @@
+﻿public class Context
+{
+    private IStrategy strategy;
+    public Context(IStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+    public int Execute(int num1, int num2)
+    {
+        return strategy.DoOperation(num1, num2);
+    }
+}
---
+++ design-patterns/DemoApp/Strategy/Strategy.cs
@@ -0,0 +1,36 @@
+﻿public interface IStrategy
+{
+    public int DoOperation(int num1, int num2);
+}
+public class OperationAdd : IStrategy
+{
+    public int DoOperation(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
+public class OperationSubtract : IStrategy
+{
+    public int DoOperation(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+}
+public class OperationMultiply : IStrategy
+{
+    public int DoOperation(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+}
+public class OperationDivide : IStrategy
+{
+    public int DoOperation(int num1, int num2)
+    {
+        if (num2 == 0)
+        {
+            throw new DivideByZeroException("Division by zero is not allowed.");
+        }
+        return num1 / num2;
+    }
+}
