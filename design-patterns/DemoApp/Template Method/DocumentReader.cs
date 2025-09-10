b95d08d912b710fe099e9881a71011515099e7be
CodeWithRSV <rsverma3333@gmail.com>
2025-09-10 11:02:02 +0530

    Added Template method pattern

---
+++ design-patterns/DemoApp/Template Method/DocumentReader.cs
@@ -0,0 +1,59 @@
+﻿abstract class DataMiner
+{
+    public void MineData()
+    {
+        this.OpenFile();
+        this.ExtractData();
+        this.ParseData();
+        this.AnalyzeData();
+        this.SendReport();
+        this.CloseFile();
+    }
+    protected void OpenFile()
+    {
+        Console.WriteLine("File open");
+    }
+
+    protected void AnalyzeData()
+    {
+        Console.WriteLine("Data analysis done");
+    }
+
+    protected void SendReport()
+    {
+        Console.WriteLine("Report sent");
+    }
+
+    protected void CloseFile()
+    {
+        Console.WriteLine("File closed");
+    }
+    protected abstract void ExtractData();
+    protected abstract void ParseData();
+}
+
+class PDFDataMiner: DataMiner
+{
+    protected override void ExtractData()
+    {
+        Console.WriteLine("Extracted Data from pdf file");
+    }
+
+    protected override void ParseData()
+    {
+        Console.WriteLine("Parsed Data from pdf file");
+    }
+}
+
+class DocDataMiner :DataMiner
+{
+    protected override void ExtractData()
+    {
+        Console.WriteLine("Extracted Data from doc file");
+    }
+
+    protected override void ParseData()
+    {
+        Console.WriteLine("Parsed Data from doc file");
+    }
+}
